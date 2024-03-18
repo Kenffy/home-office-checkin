c0e270f3222af8067145ace648af80a78c822c71
Kenffy <voufomarius@yahoo.fr>
2024-03-18 13:37:29 +0100

    add protected routes and implement persistent login

--- client/angular/src/app/app.component.ts
+++ client/angular/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,17 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
 })
 export class AppComponent {
   title = 'HomeOffice-Checkin';
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.loadCurrentUser();
+  }
+
+  loadCurrentUser() {
+    const userId = localStorage.getItem('ho-uid');
+    if (userId) {
+      this.authService.loadCurrentEmployee(userId).subscribe();
+    }
+  }
 }
--- client/angular/src/app/app.routes.ts
+++ client/angular/src/app/app.routes.ts
@@ -2,14 +2,16 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { CheckinComponent } from './checkin/checkin.component';
 import { CheckinDetailsComponent } from './checkin-details/checkin-details.component';
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/checkin', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'checkin', component: CheckinComponent },
+  { path: 'checkin', component: CheckinComponent, canActivate: [AuthGuard] },
   {
     path: 'details/:id',
     component: CheckinDetailsComponent,
+    canActivate: [AuthGuard],
   },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
